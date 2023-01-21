0cce3397c99bdde89e3f76738205d8f403cb421d
Aleticod <193837@unsaac.edu.pe>
2023-01-21 17:04:54 -0500

    Modify web.blade layout and implement index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,9 @@
-<h1>Index</h1>
+@extends('layouts.web')
+
+@section('content')
+  <div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos Cursos</h1>
+    <h2 class="text-xl text-gray-600">Formate online como profesional en tecnologia</h2>
+    <h3 class="text-lg text-gray-600">70% de los graduados duplican sus ingresos</h3>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/', 'index');
+Route::view('/', 'index')->name('home');
 
 Route::get('/welcome', function () {
     return view('welcome');
